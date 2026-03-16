b97ee6fd839c9aaab259a8f9a3ca6f70c284243b
almamun252 <252-15-949@diu.edu.bd>
2026-03-17 00:32:20 +0600

    Added Python file for Input and Output using f-string formatting

---
+++ Python basics/Input and Output using f-string formatting.py
@@ -0,0 +1,6 @@
+# Input and Output using f-string formatting
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: ")) # Input is taken as string, so we convert it to integer using int()
+
+# Formatting output with f-string
+print(f"Hello {user_name}! You are {user_age} years old.")
