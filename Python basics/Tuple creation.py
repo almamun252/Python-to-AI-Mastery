5718ea508915445e918cc273e801a6d0997ee1a8
almamun252 <252-15-949@diu.edu.bd>
2026-03-17 07:42:24 +0600

    Added Python file for tuple

---
+++ Python basics/Tuple creation.py
@@ -0,0 +1,6 @@
+# Tuple creation
+my_tuple = ("image_width", 1920, 1080)
+
+print("Tuple data:", my_tuple[1]) # Output: 1920
+
+# my_tuple[1] = 1280 # This will cause an error, because Tuples are immutable!
